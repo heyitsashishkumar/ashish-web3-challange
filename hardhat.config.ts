d2ab2c63e3db25c791611dfa650060cc0ec05b3d
Ashish Kumar <heyitsashishkumar@gmail.com>
2023-12-01 12:29:57 +0530

    changed the compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: "0.8.13",
+                version: "0.8.19",
                 settings: {
                     optimizer: {
                         enabled: true,
